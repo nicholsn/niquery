74b699e999f8b0c4824d5439fe66938210f1ec76
Nolan Nichols <nolan.nichols@gmail.com>
2012-07-23 11:15:43 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='niquery',
+    version='0.0.1',
+    author='Nolan Nichols',
+    author_email='nolan.nichols@gmail.com',
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    scripts=[],
+    url='http://pypi.python.org/pypi/niquery/',
+    license='Apache 2.0',
+    description='Framework to query neuroimaging databases.',
+    long_description=open('README').read(),
+    install_requires=["Pyro4 >= 4.0.0", "requests", "numpy"],
+)
