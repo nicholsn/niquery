2e7d660ed963814f27807d6c57d3541845348675
Nolan Nichols <nolan.nichols@gmail.com>
2012-07-24 19:22:21 -0700

    added a workflow plugin

---
+++ niquery/plugins/workflow.py
@@ -0,0 +1,11 @@
+__author__ = 'nolan'
+
+# plugin framework adapted from http://martyalchin.com/2008/jan/10/simple-plugin-framework/
+
+from ..base.plugin import WorkflowProvider
+
+class Registration(WorkflowProvider):
+    def __init__(self):
+        pass
+
+
