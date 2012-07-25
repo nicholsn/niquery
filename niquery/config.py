59c9238f7f7610aaf2b15af719aec5081ea75d94
Nolan Nichols <nolan.nichols@gmail.com>
2012-07-24 19:27:25 -0700

    modified config to support plugins

--- niquery/config.py
+++ niquery/config.py
@@ -2,6 +2,8 @@ __author__ = 'nolan'
 
 # this will be used as the main configuration for a service
 
+from .plugins.filemapper import XnatFileMapper
+
 class Config(object):
     """
     Configuration options for NIQuery service.
@@ -26,3 +28,7 @@ class Config(object):
         self.METADATA_URL = "http://axon.biostr.washington.edu/niq/stanford/xcede-nims.xml"
         # meant to be xml description of services, uri, generated?
         self.SERVICES = "<niq>test niq</niq>"
+
+        self.PLUGINS = XnatFileMapper
+
+# base class for "FileMapping" extended by sever specific
