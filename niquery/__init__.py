e8dde6ba63f3cb53058bf297f3db2c30af9626f4
Nolan Nichols <nolan.nichols@gmail.com>
2013-09-10 16:02:34 -0700

    add: api module based on Flask

---
+++ niquery/__init__.py
@@ -0,0 +1,1 @@
+__author__ = 'Nolan Nichols <nolan.nichols@gmail.com'
---
+++ niquery/api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask.ext import restful
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
