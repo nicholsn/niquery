73ec9b9d947fe1cfbf69a909e4209bbc02516a6c
Nolan Nichols <nolan.nichols@gmail.com>
2013-09-10 14:28:04 -0700

    rf: removal of all outdated niquery code

--- niquery/plugins/__init__.py
+++
@@ -1,2 +0,0 @@
-__author__ = 'nolan'
-__all__ = ['workflow','filemapper','simpletime']
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='niquery',
-    version='0.0.1',
+    version='0.0.2',
     author='Nolan Nichols',
     author_email='nolan.nichols@gmail.com',
     packages=find_packages(exclude=["tests.*", "tests"]),
     scripts=[],
     url='http://pypi.python.org/pypi/niquery/',
     license='Apache 2.0',
-    description='Framework to query neuroimaging databases.',
+    description='Framework to query neuroimaging databases as RDF.',
     long_description=open('README').read(),
-    install_requires=["Pyro4", "requests", "numpy", "nibabel", 'web'],
+    install_requires=["Flask", "Flask-RESTful", "requests"],
 )
