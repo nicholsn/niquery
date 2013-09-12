a7f4946cd0791926eea7f1113c57ec9d287303cc
Nolan Nichols <nolan.nichols@gmail.com>
2013-09-11 17:07:51 -0700

    add: flask restful and rdflib skeleton app

--- niquery/api.py
+++ niquery/api.py
@@ -1,14 +1,41 @@
-from flask import Flask
-from flask.ext import restful
+import json
+from flask import Flask, jsonify, request
+from flask.ext.restful import Api, Resource
+
+from rdflib import Graph, URIRef, Namespace
 
 app = Flask(__name__)
-api = restful.Api(app)
+api = Api(app)
 
-class HelloWorld(restful.Resource):
+
+class ROOT(Resource):
     def get(self):
-        return {'hello': 'world'}
+        dbo = Namespace('http://dbpedia.org/ontology/')
 
-api.add_resource(HelloWorld, '/')
+        graph = Graph(store='SPARQLStore')
+
+        graph.open("http://dbpedia.org/sparql")
+
+        query = u'CONSTRUCT { <http://dbpedia.org/resource/Berlin> <http://dbpedia.org/ontology/populationTotal> ?o } \
+                   WHERE { <http://dbpedia.org/resource/Berlin> <http://dbpedia.org/ontology/populationTotal> ?o }'
+
+        result = graph.store.query(query)
+
+        response = json.loads(result.serialize(format='json-ld'))
+
+        return jsonify(response)
+
+api.add_resource(ROOT, '/')
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {'status': 404,
+               'message': 'Not Found: ' + request.url,
+               }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
