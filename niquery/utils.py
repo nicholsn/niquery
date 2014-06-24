52719af556b26ae9e2d6f3895224b559ca711e39
Nolan Nichols <nolan.nichols@gmail.com>
2014-06-24 15:39:19 -0700

    add: sparql utility functions

--- niquery/utils.py
+++ niquery/utils.py
@@ -2,8 +2,32 @@ __author__ = 'Nolan Nichols <http://orcid.org/0000-0003-1099-3328>'
 
 import os
 
+import rdflib
 
-def get_sparql_meta():
-    ttl = os.path.join(os.path.dirname(__file__), 'sparql', 'meta.ttl')
-    rq = os.path.join(os.path.dirname(__file__), 'sparql', 'meta.rq')
+
+# Access namespace objects as attributes
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+
+# available namespaces
+NS = AttrDict(prov=rdflib.Namespace("http://www.w3.org/ns/prov#"),
+              nidm=rdflib.Namespace("http://www.incf.org/ns/nidash/nidm#"))
+
+_sparql_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sparql')
+
+
+def get_sparql_meta_files():
+    ttl = os.path.join(_sparql_path, 'meta.ttl')
+    rq = os.path.join(_sparql_path, 'meta.rq')
     return ttl, rq
+
+
+def get_sparql_queries():
+    queries = dict()
+    for rq_file in os.listdir(_sparql_path):
+        if rq_file.endswith('.rq'):
+            rq_path = os.path.join(_sparql_path, rq_file)
+            with open(rq_path) as rq:
+                queries.update({rq_file: rq.read()})
+    return queries
