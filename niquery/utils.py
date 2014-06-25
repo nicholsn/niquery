3032cdbcb884270744ac12351c7707bade228ad4
Nolan Nichols <nolan.nichols@gmail.com>
2014-06-24 18:26:04 -0700

    add: method to convert sparql result to dataframe

--- niquery/utils.py
+++ niquery/utils.py
@@ -1,8 +1,10 @@
 __author__ = 'Nolan Nichols <http://orcid.org/0000-0003-1099-3328>'
 
 import os
+import StringIO
 
 import rdflib
+import pandas as pd
 
 
 # Access namespace objects as attributes
@@ -19,12 +21,6 @@ NS = AttrDict(nidm=rdflib.Namespace("http://www.incf.org/ns/nidash/nidm#"),
 _sparql_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'sparql')
 
 
-def get_sparql_meta_files():
-    ttl = os.path.join(_sparql_path, 'meta.ttl')
-    rq = os.path.join(_sparql_path, 'meta.rq')
-    return ttl, rq
-
-
 def get_sparql_queries():
     queries = dict()
     for rq_file in os.listdir(_sparql_path):
@@ -33,3 +29,8 @@ def get_sparql_queries():
             with open(rq_path) as rq:
                 queries.update({rq_file: rq.read()})
     return queries
+
+
+def result_to_dataframe(query_result):
+    csv = StringIO.StringIO(query_result.serialize(format='csv'))
+    return pd.read_csv(csv)
