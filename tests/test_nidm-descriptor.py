758ef99223973f823c9813e9e98b78ea0b6eac03
Nolan Nichols <nolan.nichols@gmail.com>
2014-06-25 07:27:16 -0700

    tst: project summary level required attributes

--- tests/test_nidm-descriptor.py
+++ tests/test_nidm-descriptor.py
@@ -1,23 +1,36 @@
 __author__ = 'Nolan Nichols <http://orcid.org/0000-0003-1099-3328>'
 
 import os
+import urlparse
 import unittest
 
 import niquery as niq
 
 
 class DescriptorCase(unittest.TestCase):
+    """
+    NIDM Dataset Descriptor Validation
+    """
     def setUp(self):
         self.turtle = os.path.join(os.path.dirname(__file__), 'data', 'nidm-descriptor.ttl')
-        self.query = niq.SelectQuery()
+        self.select = niq.SelectQuery()
 
-    def test_summary_must(self):
+    def test_project_summary_must(self):
         """
-        NIDM Descriptors MUST return one record the following elements to be a valid summary level description.
+        Project Summary-level description that MUST be present.
         """
-        os.path.join(os.path.dirname(__file__), '..', 'sparql')
-
-        self.assertEqual(True, True)
+        test_query = 'nidm-descriptor-database-summary-must.rq'
+        test_filter = self.select.sparql_meta.downloadURL.str.contains(test_query)
+        test_record = self.select.sparql_meta[test_filter]
+        idx = test_record.index.to_native_types()[0]
+        result = self.select.execute(idx, turtle_file=self.turtle)
+        # check that each of the required elements are present
+        self.assertEqual(urlparse.urlparse(result.uri[0]).fragment, "database")
+        self.assertEqual(result.title[0], "Database Summary Title")
+        self.assertEqual(result.description[0], "Database summary-level description.")
+        self.assertEqual(result.type[0], "http://purl.org/dc/dcmitype/Dataset")
+        self.assertEqual(result.publisher[0],"https://publisher.org")
+        self.assertEqual(urlparse.urlparse(result.hasPart[0]).fragment, "project")
 
 
 if __name__ == '__main__':
