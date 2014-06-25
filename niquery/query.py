bb44678239ce35e3cdecbd8e4cfec353d72015d0
Nolan Nichols <nolan.nichols@gmail.com>
2014-06-24 18:26:58 -0700

    upd: execute to return dataframe

--- niquery/query.py
+++ niquery/query.py
@@ -3,6 +3,8 @@ The query module provides an object to access SPARQL queries over PROV and NI-DM
 """
 __author__ = 'Nolan Nichols <http://orcid.org/0000-0003-1099-3328>'
 
+import os
+import urlparse
 import StringIO
 
 import rdflib
@@ -18,35 +20,43 @@ class QueryBase(object):
     """
     def __init__(self):
         self._graph = rdflib.Graph()
+        self._queries = utils.get_sparql_queries()
         self._bind_prefixes()
         self.sparql_meta = self._get_sparql_meta()
 
     def _get_sparql_meta(self):
-        sparql_meta = utils.get_sparql_meta_files()
-        self._graph.parse(sparql_meta[0], format='turtle')
-        with open(sparql_meta[1]) as query:
-            result = self._graph.query(query.read())
-            meta = StringIO.StringIO(result.serialize(format='csv'))
-            return pd.read_csv(meta)
+        self._graph.parse(self._queries['meta.ttl'], format='turtle')
+        result = self._graph.query(self._queries['meta.rq'])
+        return utils.result_to_dataframe(result)
 
     def _bind_prefixes(self):
         for prefix, namespace in utils.NS.iteritems():
             self._graph.bind(prefix, namespace)
 
+    def _get_query_string(self, index):
+        queries = utils.get_sparql_queries()
+        row = self.sparql_meta.iloc[index]
+        base = os.path.basename(urlparse.urlsplit(row.downloadURL).path)
+        return queries[base]
+
     def get_graph(self):
         return self._graph
+
+    def describe_query(self, index):
+        return self.sparql_meta.iloc[index]
 
 
 class SelectQuery(QueryBase):
     def __init__(self):
         super(SelectQuery, self).__init__()
+        self.sparql_meta = self._filter_queries()
 
-    def get_queries(self):
-        queries = self.sparql_meta
-        return queries
+    def _filter_queries(self):
+        select_filter = self.sparql_meta.format == str(utils.NS.niq.Select)
+        return self.sparql_meta[select_filter]
 
-    def execute(self, turtle_file, query_url):
-        query = requests.get(query_url)
-        self._graph.parse(turtle_file, format='turtle')
-        result = self._graph.query(query.text)
-        return result
+    def execute(self, query_index, turtle_file=None, turtle_url=None):
+        query = self._get_query_string(query_index)
+        self._graph.parse(source=turtle_file, location=turtle_url, format='turtle')
+        result = self._graph.query(query)
+        return utils.result_to_dataframe(result)
