37d213b22b4a57b300c6e491d03d7e45ed2cb56f
Nolan Nichols <nolan.nichols@gmail.com>
2014-08-20 12:33:48 -0700

    doc: method doc strings

--- niquery/query.py
+++ niquery/query.py
@@ -25,29 +25,56 @@ class QueryBase(object):
         self.sparql_meta = self._get_sparql_meta()
 
     def _bind_prefixes(self):
+        """
+        Loads the base set of namespaces into rdflib
+        """
         for prefix, namespace in utils.NS.iteritems():
             self._graph.bind(prefix, namespace)
 
     def _filter_queries(self, ns):
+        """
+        Enables subclasses to filter queries by type (e.g., SELECT queries are
+        filtered using utils.NS.niq.Select.
+        """
         select_filter = self.sparql_meta.format == str(ns)
         return self.sparql_meta[select_filter]
 
     def _get_query_string(self, index):
+        """
+        Extracts the actual query text to execute, based on the query index.
+        """
         queries = utils.get_sparql_queries()
         row = self.sparql_meta.iloc[index]
         base = os.path.basename(urlparse.urlsplit(row.downloadURL).path)
         return queries[base]
 
     def _get_sparql_meta(self):
+        """
+        Parses the query metadata and load a table of available queries.
+        """
         self._graph.parse(os.path.join(utils.get_meta_path()),
                           format='turtle')
         result = self._graph.query(self._queries['meta.rq'])
         return utils.result_to_dataframe(result)
 
     def describe_query(self, index):
+        """
+        Uses the index of a query to return a description
+        """
         return self.sparql_meta.iloc[index]
 
+    def execute(self, query_index, turtle_file=None, turtle_url=None):
+        """
+        Execute a query using the index
+
+        Subclasses define specific parameters and returns
+        """
+        pass
+
     def get_graph(self):
+        """
+        Returns the currently loaded graph
+        """
         return self._graph
 
 
@@ -57,6 +84,20 @@ class SelectQuery(QueryBase):
         self.sparql_meta = self._filter_queries(utils.NS.niq.Select)
 
     def execute(self, query_index, turtle_file=None, turtle_url=None):
+        """
+        Execute a query using the index
+
+        Parameters
+        ----------
+        query_index : int
+        turtle_file : str, optional
+        turtle)url : string, optional
+
+        Returns
+        -------
+        df : pandas.Dataframe
+            Dataframe object of SELECT SPARQL query
+        """
         query = self._get_query_string(query_index)
         self._graph.parse(source=turtle_file,
                           location=turtle_url,
